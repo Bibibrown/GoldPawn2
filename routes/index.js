b10ba14d630b0d36b9ef30cbf199ae9d428b742a
Punnita Pethphukdee <punnita.p@kkumail.com>
2024-10-04 01:43:51 +0700

    change

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Customer = require('../models/customer'); // โมเดลลูกค้า
+const GoldPawn = require('../models/goldPawn'); // โมเดลทอง
 
+// GET หน้าแรก
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+// GET หน้ารายชื่อลูกค้า
+router.get('/customer-list', async (req, res) => {
+    try {
+        const customers = await Customer.find(); // ดึงข้อมูลลูกค้าทั้งหมด
+        res.render('customerlist', { customers }); // ส่งข้อมูลไปยัง view
+    } catch (error) {
+        console.error('Error fetching customers:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// GET หน้าข้อมูลทอง
+router.get('/gold-list', async (req, res) => {
+    try {
+        const goldData = await GoldPawn.find().populate('typeName', 'typeName'); // ดึงข้อมูลทองทั้งหมด พร้อม populate typeName
+        res.render('goldlist', { goldData }); // ส่งข้อมูลทองไปยัง view
+    } catch (error) {
+        console.error('Error fetching gold data:', error);
+        res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
